03d4eb0cd863272d2b4fc22a3d89421edd7b33cc
anzioui <766168543@qq.com>
2021-08-28 11:17:09 +0800

    My second commit

--- src/main/java/com/gan/git/GitTest.java
+++ src/main/java/com/gan/git/GitTest.java
@@ -3,5 +3,6 @@ package com.gan.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello my git ");
+        System.out.println("hello my git ");
     }
 }
